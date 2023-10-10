ec0c6e26ada8249299881ec35841ea32fed6a42e
manishkandpal1 <manishkandpal003@gmail.com>
2023-10-10 19:17:55 +0530

    -

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,12 @@
+const ServerIdLayout=({
+    children
+}:{
+    children:React.ReactNode;
+})=>{
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
+export default ServerIdLayout;
---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,6 @@
+const ServerIdPage=()=>{
+    return (
+        <div>Server Id page</div>
+    )
+}
+export default ServerIdPage;
