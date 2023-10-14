11e34a0628f1e8db30d17683fc0fda04dafa8fb7
manishkandpal1 <manishkandpal003@gmail.com>
2023-10-14 22:11:27 +0530

    -

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,27 +1,27 @@
-import {redirect} from "next/navigation";
+import { redirect } from "next/navigation";
 
-import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
 import { InitialModal } from "@/components/modals/initial-modal";
 
-const SetUpPage = async() => {
-    const profile=await initialProfile();
-    const server=await db.server.findFirst({
-        where:{
-            member:{
-                some:{
-                    profileId:profile.id
-                }
-            }
-        }
-    });
+const SetupPage = async () => {
+  const profile = await initialProfile();
 
-    if(server){
-        return redirect(`/servers/${server.id}`);
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
     }
-    return ( 
-        <InitialModal/>
-     );
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <InitialModal />;
 }
  
-export default SetUpPage;
+export default SetupPage;
